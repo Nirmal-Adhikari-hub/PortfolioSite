d66ef8b183e6b26488265f5ae364f7b4900edf07
Nirmal Adhikari <adhikareen@inha.edu>
2025-06-30 13:40:52 +0900

    Refine dark theme and blog aesthetics

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,5 +1,7 @@
 
 import type { Config } from "tailwindcss";
+import tailwindcssAnimate from "tailwindcss-animate";
+import typography from "@tailwindcss/typography";
 
 export default {
 	darkMode: ["class"],
@@ -93,8 +95,8 @@ export default {
 			}
 		}
 	},
-	plugins: [
-		require("tailwindcss-animate"),
-		require("@tailwindcss/typography")
-	],
+       plugins: [
+                tailwindcssAnimate,
+                typography,
+       ],
 } satisfies Config;
